fc12ccb261640e7de9aba81ff6104187bffd2c8e
storngfornt <au0038173@gmail.com>
2024-11-26 15:24:32 +0600

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -64,7 +64,9 @@ const Hero = () => {
                         <a href="https://www.upwork.com/freelancers/~013cbd127b8d820d40" target='_blank'>
                         <Button  bg={'bg-[#00e676]'} size='p-3' text={<FaUpwork className='w-4 h-4' />} />
                         </a>
+                        <a href="https://github.com/Raihan-24-26" target='_blank'>
                         <Button  bg={'bg-[#00e676]'} size='p-3' text={<FaGithub className="w-4 h-4"/>} />
+                        </a>
                     </div>
                 </div>
 
